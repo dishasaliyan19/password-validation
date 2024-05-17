133b206b463da556bd38af2b25c257cd83602a37
dishasaliyan19 <dishasaliyan59@gmail.com>
2024-05-17 10:22:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function matchpassword(){
+    var firstPass=document.f1.password.value;
+    var secondPass=document.f1.password2.value;
+    if(firstPass==secondPass){
+        alert("your password is correct");
+        return true;
+    }
+    else{
+        alert("password must be same");
+        return false;
+    }
+    }
